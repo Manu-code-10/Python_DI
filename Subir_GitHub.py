b2e96bf2ea4b267d32bf9fff5dec02e7b5d89ebe
Manu-code-10 <manuel.10benito@gmail.com>
2024-10-22 11:46:52 +0200

    Actualización 22/10/2024 11:46 desde Python

--- Subir_GitHub.py
+++ Subir_GitHub.py
@@ -3,38 +3,39 @@
 Código para subir de forma automática a github
 """
 
-import subprocess
+import subprocess as git
 from datetime import datetime
 
 # Define los comandos de git
 def git_push(repo_path):
     try:
         # Obtener la fecha actual en formato dd/mm/aaaa
-        fecha_actual = datetime.now().strftime("%d/%m/%Y")
+        fecha_actual = datetime.now().strftime("%d/%m/%Y %H:%M")
 
-        # Definir el mensaje del commit
+        # Definir el mensaje del commit (mensaje de subida)
         commit_message = f"Actualización {fecha_actual} desde Python"
 
-        # Agregar todos los archivos modificados
-        subprocess.run(["git", "add", "."], check=True, cwd=repo_path)
+        # Agregar todos los archivos modificados (solo agregamos archivos
+        # modificados después del anterior commit)
+        git.run(["git", "add", "."], check=True, cwd=repo_path)
 
         # Hacer el commit con el mensaje que incluye la fecha
-        subprocess.run(["git", "commit", "-m", commit_message], check=True,
+        git.run(["git", "commit", "-m", commit_message], check=True,
                        cwd=repo_path)
 
         # Subir los cambios a la rama principal (main)
-        subprocess.run(["git", "push", "-u", "origin", "main"], check=True,
+        git.run(["git", "push", "-u", "origin", "main"], check=True,
                        cwd=repo_path)
 
         print(f"Commit realizado con el mensaje: '{commit_message}'")
         print("Archivos subidos a GitHub correctamente.")
 
-    except subprocess.CalledProcessError as e:
+    except git.CalledProcessError as e:
         print(f"Error durante la ejecución de Git: {e}")
 
-# Ruta a tu repositorio local
+# Ruta a tu repositorio local (¡DEL ORDENADOR!)
 repo_path = "C:/Programacion/Python/"  
 
-# Llamar a la función para hacer el push
+# Llamada a la función para subir los archivos
 git_push(repo_path)
 
